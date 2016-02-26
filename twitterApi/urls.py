1af715dd00d79c5605b7eafdb68a7f011233315b
Li Xu <li-xu@outlook.com>
2016-02-26 10:34:08 -0600

    redirect root url to correct address

--- twitterApi/urls.py
+++ twitterApi/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
@@ -6,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
    url(r'^twitterApp/',include('twitterApp.urls')),
+   url(r'^$',RedirectView.as_view(url='twitterApp/'))
 )
